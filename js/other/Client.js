907bc3c1b62d08537c75d66d9331194215b1295f
maxime <maxime.samak@gmail.com>
2020-04-08 12:55:04 +0200

    update circular buffer closes #15

--- js/other/Client.js
+++ js/other/Client.js
@@ -10,7 +10,7 @@ socket.on("ping", function(ms) {
 socket.on("data", function(timeStamp, ping, serverTime) {
     document.getElementById("latency").innerText = ~~(ping / 2) + " ms";
     document.getElementById("ping").innerText = ping + " ms";
-    pings.push({x :(Date.now() - connectionStamp) / 1000, y: ping} );
+    pings.push({x :((Date.now() - connectionStamp) / 1000).toFixed(1), y: ping} );
 
     document.getElementById("server-time").innerText = serverTime / 1000 + " s";
     document.getElementById("client-time").innerText = (Date.now() - connectionStamp) / 1000 + " s";
@@ -26,22 +26,18 @@ function circularBuffer(size) {
 
     let bufferSize = size;
     let buffer = new Array(bufferSize);
-    buffer.fill({x:0, y:0});
+    buffer.fill({x:null, y:null});
 
     let last = 0;
-    let end = 0;
+    let end = buffer.push({x:0.5, y:1}); // initial push for correct chart rendering
 
     // Adds values to array in circular.
     this.push = function(obj) {
-        buffer[end] = obj;
-        if (end >= bufferSize) {
-            last = bufferSize - 1;
-            end = 0;
-        } // advance
-        else {
-            last = end;
-            end++;
+        end = buffer.push(obj);
+        if (end >= bufferSize - 1) {
+            buffer.shift()
         }
+        else { last = end - 1; }
     };
 
     // Returns a value from the buffer
@@ -53,9 +49,29 @@ function circularBuffer(size) {
         return buffer[last]
     };
 
+    this.data = function() {
+        return buffer;
+    };
+
+    this.dataX = function () {
+        let dataX = [];
+        for (let i = 0; i < bufferSize; i++) {
+            dataX.push(buffer[i].x)
+        }
+        return dataX;
+    };
+
+    this.dataY = function () {
+        let dataY = [];
+        for (let i = 0; i < bufferSize; i++) {
+            dataY.push(buffer[i].y)
+        }
+        return dataY;
+    };
+
     // Returns true if the buffer has been initialized.
     this.isInitialized = function() {
-        return buffer[0] != null;
+        return end !== 0;
     };
 }
 
